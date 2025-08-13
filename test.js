b7cf5b24ce36ac4099a49f5fcec9a6b49f01339a
Ahmed Jan <janahmed902@gmail.com>
2025-08-13 19:13:31 +0530

    tidy up

--- test.js
+++ test.js
@@ -1,13 +1,5 @@
-// const assert = require('assert');
-
-// describe('Hello World Test', () => {
-//   it('should return Hello World', () => {
-//     const output = "Hello, World";
-//     assert.strictEqual(output, "Hello, World");
-//   });
-// });
 const assert = require('assert');
-const getMessage = require('./main'); // import function from main.js
+const getMessage = require('./main');
 
 describe('Main.js Test', () => {
   it('should return Hello, World', () => {
