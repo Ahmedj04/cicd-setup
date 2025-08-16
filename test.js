42ebfbb8113d49fb3f8e0dd12a60d1283880312e
Ahmed Jan <janahmed902@gmail.com>
2025-08-16 13:22:20 +0530

    updated description

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const getMessage = require('./main');
 
 describe('Main.js Test', () => {
-  it('should return Hello, World', () => {
+  it('should return Hello, World Ahmed', () => {
     const output = getMessage();
     assert.strictEqual(output, "Hello, World Ahmed");
   });
